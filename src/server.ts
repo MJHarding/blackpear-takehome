9a2de73646794d3b198d6f4bdeda0289bf8d9c54
MJHarding <mattm08@live.co.uk>
2025-02-08 21:05:54 +0000

    feat: update server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response } from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import patientRoutes from './routes/patient.routes';
+import observationRoutes from './routes/observation.routes';
 
-dotenv.config({ path: require('find-config')('.env') });
+dotenv.config();
 
 const app = express();
-
 const port = process.env.DEV_PORT || 3000;
-console.log(process.env.DEV_PORT)
-// Define a route for the root path ('/')
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Matthews NodeJS API using Typescript!');
+  res.send('Matthews API!');
 });
 
+app.use('/api', patientRoutes, observationRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
